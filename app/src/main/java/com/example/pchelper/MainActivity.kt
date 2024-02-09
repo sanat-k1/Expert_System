4db0725133d347a9f6dbd2eebbf7701c544ef844
sanat <sanatk791@gmail.com>
2024-02-09 21:42:29 +0530

    autocomplete 0.1

--- app/src/main/java/com/example/pchelper/MainActivity.kt
+++ app/src/main/java/com/example/pchelper/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -13,12 +15,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        var questions = arrayOf("GPU","CPU","RAM")
         datalist = ArrayList()
         recyclerView = findViewById(R.id.recyclerView)
+        var qbar : AutoCompleteTextView = findViewById(R.id.editText)
+        var qadapter: ArrayAdapter<String> = ArrayAdapter<String>(this,android.R.layout.select_dialog_item,questions)
+        qbar.threshold = 1
+        qbar.setAdapter(qadapter)
 
         var db = getSharedPreferences("database",Context.MODE_PRIVATE)
         var editor = db.edit()
+
+
 
 //       checking if database exist
         if(db.getString("database","null") == "null" )
