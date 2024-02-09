808dc11d712dbf2b0967d1fbbd9a83e480e58623
sanat <sanatk791@gmail.com>
2024-02-09 22:56:43 +0530

    lgbt buttons

--- app/src/main/java/com/example/pchelper/MainActivity.kt
+++ app/src/main/java/com/example/pchelper/MainActivity.kt
@@ -1,33 +1,64 @@
 package com.example.pchelper
 
+
 import android.content.Context
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var datalist : ArrayList<QA>
+    private lateinit var qbar : AutoCompleteTextView
+    private lateinit var jif : ImageView
+    private lateinit var submit: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var questions = arrayOf("GPU","CPU","RAM")
-        datalist = ArrayList()
+
+        submit= findViewById(R.id.button)
         recyclerView = findViewById(R.id.recyclerView)
-        var qbar : AutoCompleteTextView = findViewById(R.id.editText)
+        qbar   = findViewById(R.id.editText)
+        jif= findViewById(R.id.imageView)
+
+//        datalist = ArrayList()
+        var questions = arrayOf("GPU","CPU","RAM")
+
         var qadapter: ArrayAdapter<String> = ArrayAdapter<String>(this,android.R.layout.select_dialog_item,questions)
-        qbar.threshold = 2
+        qbar.threshold = 1
         qbar.setAdapter(qadapter)
 
         var db = getSharedPreferences("database",Context.MODE_PRIVATE)
         var editor = db.edit()
 
+//        jifff
+        Glide.with(this)
+            .asGif()
+            .load(R.drawable.giphy)
+            .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE))
+            .into(jif)
+
+        val colors = intArrayOf(
+            ContextCompat.getColor(this, R.color.red),
+            ContextCompat.getColor(this, R.color.green),
+            ContextCompat.getColor(this, R.color.blue),
+            ContextCompat.getColor(this, R.color.Cyan),
+            ContextCompat.getColor(this, R.color.Magenta),
+            ContextCompat.getColor(this, R.color.yellow)
+        )
 
 
+//        val lgbtlights  = AnimationUtils.loadAnimation(this,R.anim.lgbt)
+//        submit.startAnimation(lgbtlights)
 //       checking if database exist
         if(db.getString("database","null") == "null" )
         {
