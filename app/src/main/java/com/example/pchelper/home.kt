c9fbf1ac624a2d49d45a1d961fd178628ddf104e
sanat <sanatk791@gmail.com>
2024-02-11 16:38:30 +0530

    added faggots

---
+++ app/src/main/java/com/example/pchelper/home.kt
@@ -0,0 +1,34 @@
+package com.example.pchelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.pchelper.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+
+class home : AppCompatActivity() {
+    lateinit var bottomNavigationMenuView: BottomNavigationMenuView
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+//        bottomNavigationMenuView = findViewById(R.id.bottomNavigationView)
+//        bottomNavigationMenuView.setOnClickListener {
+//            when(it.id)
+//            {
+//                R.id.Chatbot -> replaceFragment(chatbot())
+//                R.id.vish1 -> replaceFragment(fag1())
+//                R.id.vish2 -> replaceFragment(fag2())
+//            }
+//
+//        }
+//        replaceFragment(chatbot())
+    }
+//    private fun replaceFragment(fragment : Fragment){
+//
+//        val fragmentManager = supportFragmentManager
+//        val fragmentTransaction = fragmentManager.beginTransaction()
+//        fragmentTransaction.replace(R.id.fragmentxx,fragment)
+//        fragmentTransaction.commit()
+//    }
+}
