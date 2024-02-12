247ec6a38f0ac7e27957b3cd48b27a5df76643db
Vish <Vishal.is21@bmsce.ac.in>
2024-02-13 00:50:06 +0530

    added_sanat_to_about

--- app/src/main/java/com/example/pchelper/about.kt
+++ app/src/main/java/com/example/pchelper/about.kt
@@ -1,25 +1,27 @@
 package com.example.pchelper
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [about.newInstance] factory method to
- * create an instance of this fragment.
- */
 class about : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,12 +30,19 @@ class about : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+    // Function to open email client
+    fun composeEmail(email: String) {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:$email")
+            // Set Gmail package name to ensure email is sent using Gmail
+            setPackage("com.google.android.gm")
+        }
+        if (intent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivity(intent)
+        } else {
+            // Handle case where Gmail app is not available
+            // You can prompt the user to install Gmail or use a different email client
+        }
     }
+
 }
